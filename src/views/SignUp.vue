b2c691b2ce8ab9e6d6bdcefd318bf8674a2fc3a4
Carl-code <arimatea865@gmail.com>
2020-02-10 14:08:57 -0600

    CREATE: SignUp view and add Styles

---
+++ src/views/SignUp.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="sign-up">
+        <p>Let´s create a new Account!</p>
+        <input type="text" placeholder="Email"><br>
+        <input type="password" placeholder="Password"><br>
+        <button>Sign Up</button>
+        <span>or go back to login.</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'signUp',
+    data() {
+        return {}
+    },
+    methods: {}
+}
+</script>
+
+<style scoped>
+    .signUp {
+        margin-top: 40px;
+    }
+    input {
+        margin: 10px 0;
+        width: 20%;
+        padding: 15px;
+    }
+
+    button {
+        margin-top: 10px;
+        width: 10%;
+        cursor: pointer;
+    }
+
+    span {
+        display: block;
+        margin-top: 20px;
+        font-size: 11px;
+    }
+</style>
