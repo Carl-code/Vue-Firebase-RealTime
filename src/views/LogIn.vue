328fee4fcf42636f83785d64f31cfe03068aca43
Carl-code <arimatea865@gmail.com>
2020-02-10 14:21:52 -0600

    ADD: router-link to the direction create account and go back

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -4,7 +4,7 @@
         <input type="text" placeholder="Email"><br>
         <input type="password" placeholder="Password"><br>
         <button>Connection</button>
-        <p>You don´t have an account? You can create one</p>
+        <p>You don´t have an account? You can <router-link to="/sign-up">create one</router-link></p>
     </div>
 </template>
 
