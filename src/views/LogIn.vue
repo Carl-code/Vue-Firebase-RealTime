a580a29be792b1b9880fdfdc4bc274e8e023d60d
Carl-code <arimatea865@gmail.com>
2020-02-10 13:39:09 -0600

    ADD: LogIn view

---
+++ src/views/LogIn.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="login">
+        <h3>Sign In</h3>
+        <input type="text" placeholder="Email"><br>
+        <input type="password" placeholder="Password"><br>
+        <button>Connection</button>
+        <p>You don´t have an account? You can create one</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login',
+    data() {
+        return {};
+    },
+    methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
